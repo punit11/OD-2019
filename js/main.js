3e4c41ef04277fc6af307f245bb4259b8a999b92
Daniel Azzopardi <d.azzopardi@deakin.edu.au>
2019-05-13 17:48:52 +1000

    PWP-8482 - faqs and video added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,32 @@
 
     $("button.accordion").click(function(){
       $(this).toggleClass("active");
+      if($(this).hasClass('active')) {
+        $(this).find('.button-text').text('Hide');
+      } else {
+        $(this).find('.button-text').text('Show');
+      }
     });
+
+    // Initialise modalVideo to play button
+    $(".js-modal-btn").modalVideo({
+      youtube:{
+        autoplay:1,
+				controls:0,
+        nocookie: true, 
+			}
+    });
+
+    // On click of button, make the page not scroll.
+    $(".js-modal-btn").on('click', function(){
+      $('html').addClass('modal-noscroll');
+    })
+
+    // Make site scroll again once video is closed.
+    $(document).on('click','.js-modal-video-dismiss-btn', function(){
+      if($('html').hasClass('modal-noscroll')) {
+        $('html').removeClass('modal-noscroll');
+      }
+    })
 
   }); // on-ready end
