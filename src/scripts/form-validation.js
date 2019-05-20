aadb52d89fbb3b64ebd7879887bafb2f90a6f736
Puneet Sharma <p.sharma@deakin.edu.au>
2019-05-20 16:47:00 +1000

    Webpack configured/added

---
+++ src/scripts/form-validation.js
@@ -0,0 +1,98 @@
+import $ from "jquery";
+
+const form_validation = function(){
+var iAmA_val = '';
+$("input[name='form-lead-type']").change(function(){
+    iAmA_val = $( "input[name='form-lead-type']:checked" ).val();
+    if (iAmA_val === 'School Leaver') {
+        iAmA_val = true;
+        $('.iYL').show();
+        $('.study-level').hide();
+    } else if ((iAmA_val === 'TAFE Student') || (iAmA_val === 'University Student') || (iAmA_val === 'Employed')) {
+        $('.iYL').hide();
+        $('.study-level').show();
+    }
+    else {
+        $('.iYL,.study-level').hide();
+    }
+});
+
+$("#comps-form").validate({
+    ignore: ":hidden",
+    rules: {
+        "form-name": "required",
+        "form-mobile": {
+            required: true,
+            number: true,
+            minlength: 10,
+            maxlength: 10
+        },
+        "form-email": {
+            required: true,
+            email: true
+        },
+        "form-lead-type": "required",
+        "form-year-level": {
+            required: function(element) {
+                let inputIama_val = $("input[name='form-lead-type']:checked").val();
+                return (inputIama_val === "School Leaver");
+            }
+        },
+        inputStudyLevel: {
+            required: function(element) {
+                let inputIama_val = $("input[name='form-lead-type']:checked").val();
+                return ((inputIama_val === "TAFE Student") || (inputIama_val === "University Student") || (inputIama_val === "Employed"));
+            }
+        },
+        "form-ia": { 
+            required: true, 
+            minlength: 1 
+            },
+        "form-location": { 
+            required: true, 
+            minlength: 1 
+            }
+        },
+    messages: {
+        "form-name": "(Please enter your name)",
+        "form-mobile": {
+            required: "(Please enter your phone number)",
+            number: "(Please enter numbers only)",
+            minlength: "(Phone number should be 10 digit long)",
+            maxlength: "(Phone number should not be more than 10 digits)"
+        },
+        "form-email": {
+            required: "(Please enter your email)",
+            email: "(Please enter a valid email)"
+        },
+        "form-lead-type": "(Please select your status)",
+        "form-year-level": "(Please select an year level)",
+        "inputStudyLevel": "(Please select a study level)",
+        "form-ia": "(Please select atleast one interest area)",
+        "form-location": "(Please select atleast one campus)"
+    },
+    errorPlacement: function (error, element) {
+        console.log('errors - ',error);
+        if (element.attr("name") == "form-name")
+            error.insertAfter("#form-name");
+        else if (element.attr("name") == "form-mobile")
+            error.insertAfter("#form-mobile");
+        else if (element.attr("name") == "sign-up-email")
+            error.insertAfter("label[for='reg_email'] + span");
+        else if (element.attr("name") == "form-lead-type")
+            error.insertAfter('.form-stutype h3');
+        else if (element.attr("name") == "form-year-level")
+            error.insertAfter('.iYL h3');
+        else if (element.attr("name") == "inputStudyLevel")
+            error.insertAfter('.study-level h3');
+        else if (element.attr("name") == "form-ia")
+            error.insertAfter('.form-title-ia h3');
+        else if (element.attr("name") == "form-location")
+            error.insertAfter('.form-title-location h3');
+        else
+            error.insertAfter(element);
+    }
+});
+};
+
+export {form_validation};
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,55 @@
+const Path = require("path");
+const Webpack = require("webpack");
+const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const common = require("./webpack.common.js");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  stats: "errors-only",
+  bail: true,
+  output: {
+    // filename: "js/[name].[chunkhash:8].js",
+    // chunkFilename: "js/[name].[chunkhash:8].chunk.js"
+    filename: "js/[name].bundle.js",
+    chunkFilename: "js/[name].chunk.js"
+  },
+  optimization:{
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()] // minify css & JS files on PROD
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    }),
+    new Webpack.optimize.ModuleConcatenationPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].bundle.css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // 3. Extract css into files
+          "css-loader?sourceMap=true", // 2. Turn css into commonjs
+          "sass-loader", // 1. Turn sass into css
+          {
+            loader: "sass-resources-loader", // Make SASS variables/mixins avalaibe in other SASS files
+            options: {
+              resources: ["./src/styles/_variables.scss", "./src/styles/_mixins.scss","./src/styles/_common.scss"]
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
