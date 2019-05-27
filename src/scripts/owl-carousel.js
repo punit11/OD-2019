fb55329488ad667ab93ae288e521f6a5aa95affd
danazz09 <d.azzopardi@deakin.edu.au>
2019-05-27 16:20:04 +1000

    fix mobile nav

--- src/scripts/owl-carousel.js
+++ src/scripts/owl-carousel.js
@@ -4,28 +4,30 @@ import {owlCarousel} from "owl.carousel";
  
  // Make eventcards into a carousel
  const owl = $(function() {
-     $(".owl-carousel").owlCarousel({
-    center: true,
-    loop:true,
-    margin:10,
-    nav: true,
-    stagePadding: 0,
-    heightStyle: "content",
-    // responsiveClass:true,
-    responsive:{
-      0:{
-        items:1
-      },
-      576: {
-        items: 2
-      },
-      768: {
-        items: 2.25
-      },
-      1200:{
+  $(window).on('load', function(){
+    $(".owl-carousel").owlCarousel({
+      center: true,
+      loop:true,
+      margin:10,
+      nav: true,
+      stagePadding: 0,
+      heightStyle: "content",
+      // responsiveClass:true,
+      responsive:{
+        0:{
+          items:1
+        },
+        576: {
+          items: 2
+        },
+        768: {
+          items: 2.25
+        },
+        1200:{
           items:2.5
+        }
       }
-    }
+    });
   });
 });
 export {owl};
