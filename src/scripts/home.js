95941e973ffe2123e97ca28de1ddddbfb51bebd0
danazz09 <d.azzopardi@deakin.edu.au>
2019-05-30 13:07:07 +1000

    more edits

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import { brotliDecompressSync } from "zlib";
 
 var navbar_toggle = (function() {
   $(".Navbar__Link-toggle").on("click", function() {
@@ -20,6 +21,17 @@ var accordion_click = (function() {
     }
   });
 }());
+
+// $('.Navbar__Link a').on('click', function(e){
+//   e.preventDefault();
+
+//   let link = $(this).attr("href");
+//   console.log('link = ' + link);
+//   let goto = $("a[name='" + link + "']");
+//   console.log(goto);
+
+//   $("html").animate({ scrollTop: goto.offset().top }, 500);
+// });
 
 // Make site scroll again once video is closed.
 // $(document).on(
