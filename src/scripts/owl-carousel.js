c819c018788ec5461d2d17f578ce1b26aec46d8a
danazz09 <d.azzopardi@deakin.edu.au>
2019-06-14 12:25:18 +1000

    youtube locations added and some styling issues fixed

--- src/scripts/owl-carousel.js
+++ src/scripts/owl-carousel.js
@@ -10,7 +10,19 @@ let tours_cards_len = $(".tours-exp-carousel .eventcard").length;
 let gen_info_cards_len = $(".gen-info-carousel .eventcard").length;
 
   var owl = $('.owl-carousel');
-  
+
+  // $(document).on('click','.owl-nav button', function(event) {
+  //   console.log('owl carousel change');
+  //   $('.owl-item.active.fadeOut').removeClass('fadeOut');
+  //     var activeEls = $('.owl-item.active').eq(2); 
+  //     activeEls.addClass('fadeOut'); 
+  // });
+
+  // $(document).on('click', '.owl-nav button', function(event) {
+  //   fadeTile();
+  // });
+
+
   owl.removeClass("owl-hidden");
   owl.owlCarousel('destroy'); // Kill owl first on click before re-init
   owl.fadeIn('slow'); // Unhide cards before re-init
@@ -26,9 +38,9 @@ let gen_info_cards_len = $(".gen-info-carousel .eventcard").length;
 			0: {items: 1},
 			600: {items: 2},
 			1000: {items: 2.5,
-            loop:(course_cards_len > 3) ? true: false,
-            dotsEach: (course_cards_len > 3) ? 3: 1,
-            slideBy: (course_cards_len > 3) ? 3: 1
+            loop:(course_cards_len > 2) ? true: false,
+            dotsEach: (course_cards_len > 2) ? 2: 1,
+            slideBy: (course_cards_len > 2) ? 2: 1
 			}
 		}
   });
@@ -45,9 +57,9 @@ let gen_info_cards_len = $(".gen-info-carousel .eventcard").length;
 			0: {items: 1},
 			600: {items: 2},
 			1000: {items: 2.5,
-            loop:(tours_cards_len > 3) ? true: false,
-            dotsEach: (tours_cards_len > 3) ? 2: 1,
-            slideBy: (tours_cards_len > 3) ? 2: 1
+            loop:(tours_cards_len > 2) ? true: false,
+            dotsEach: (tours_cards_len > 2) ? 2: 1,
+            slideBy: (tours_cards_len > 2) ? 2: 1
 
 			}
 		}
@@ -65,7 +77,7 @@ let gen_info_cards_len = $(".gen-info-carousel .eventcard").length;
 			0: {items: 1},
 			600: {items: 2},
 			1000: {items: 2.5,
-            loop:(gen_info_cards_len > 3) ? true: false,
+            loop:(gen_info_cards_len > 2) ? true: false,
             dotsEach: (gen_info_cards_len > 2) ? 2: 1,
             slideBy: (gen_info_cards_len > 2) ? 2: 1
 			}
@@ -73,4 +85,22 @@ let gen_info_cards_len = $(".gen-info-carousel .eventcard").length;
   });
 } // end owl bind
 
+// $(function() {
+// function fadeTile(event){
+  // console.log('fadeTile function initialised');
+  
+  
+  // $('.owl-carousel').on("ready", ".owl-stage .owl-item.active", function() {
+  //   $('.owl-item.active.fadeOut').removeClass('fadeOut');
+  // });
+  // console.log('activeEls ', activeEls);
+  // activeEls.addClass('fadeOut'); 
+  // $( ".owl-carousel" ).each(function() {
+  //   var activeEls = $('.owl-item.active').eq(2); 
+  //   activeEls.addClass('fadeOut'); 
+  //   console.log('activeEls ', activeEls);
+  // });
+// }
+// fadeTile();
+// });
 export default bind_owl;
