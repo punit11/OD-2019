6b39cf2a2eea1e3d49f8e0b8145a3e8b11a36f9b
danazz09 <d.azzopardi@deakin.edu.au>
2019-06-27 09:26:52 +1000

    multiple fixes

--- src/scripts/journey/journey.js
+++ src/scripts/journey/journey.js
@@ -6,17 +6,18 @@ var journey = $(function() {
         show_hide_modal();
 
         var contentHeight = $(".modal-content").height(),
-            pageHeight = $(window).outerHeight(),
-            maxContentHeight = pageHeight - 100;
+        pageHeight = $(window).outerHeight(),
+        maxContentHeight = pageHeight - 100;
 
+        console.log("pageheight = " + pageHeight);
         if($(window).height() > 550) {
+            console.log("window height > 500");
             $(".modal-content").css('max-height', maxContentHeight + 'px');
         } else {
+            console.log("window height < 500");
             $(".modal-content").css('max-height', '500px');
         }
-    });
-
-    
+    });    
 }());
 
 export {journey};
